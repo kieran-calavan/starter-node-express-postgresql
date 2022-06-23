ee3cbf6c7d325988fe587f3250166cf015941783
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2022-06-23 16:08:49 -0500

    updates to .json

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,12 @@ const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
-
 module.exports = {
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
     migrations: {
-      direcotry: path.join(__dirname, "src", "db", "migrations"),
-      // Setting directory: path.join(__dirname, "src", "db", "migrations") within the migrations object will tell Knex to store migration files in the migrations folder at src/db/migrations.
-    }
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
   },
 };
