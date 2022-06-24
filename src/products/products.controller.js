817a4954adc74832c6ac0e0a5adb90d89638086f
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2022-06-24 09:54:39 -0500

    catagories and products updates

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,11 +1,27 @@
-function read(req, res, next) {
-  res.json({ data: { product_title: "some product title" } });
+const productsService = require("./products.service")
+
+function read(productId) {
+  return knex("products").select("*").where({ product_id: productId }).first();
 }
 
 function list(req, res, next) {
-  res.json({
-    data: [{ product_title: "product 1" }, { product_title: "product 2" }],
-  });
+  productsService
+    .list()
+    .then((data) => res.json({ data }))
+    .catch(next);
+}
+
+function productExists(req, res, next) {
+  productsService
+    .read(req.params.productId)
+    .then((product) => {
+      if (product) {
+        res.locals.product = product;
+        return next();
+      }
+      next({ status: 404, message: `Product cannot be found.` });
+    })
+    .catch(next);
 }
 
 module.exports = {
